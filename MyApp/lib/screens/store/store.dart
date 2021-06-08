d27f4945ba438905c0da4993f722b9fc9a24f456
HaiNamNguyen3012 <47827233+HaiNamNguyen3012@users.noreply.github.com>
2021-06-08 12:36:42 +0700

    Update store.dart

--- MyApp/lib/screens/store/store.dart
+++ MyApp/lib/screens/store/store.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:movie_ticket_app/screens/store/Store_screen.dart';
 class Store extends StatefulWidget {
   const Store({Key key}) : super(key: key);
 
